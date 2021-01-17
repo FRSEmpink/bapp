af71c71025f54a291fa66230ab73346bf8c3244a
VEmpink <fiermandt@gmail.com>
2021-01-17 10:43:37 +0700

    Extract the "bapp.zip" 🍱

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace bapp
+{
+    partial class bapp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(bapp));
+            this.scanApp = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // scanApp
+            // 
+            this.scanApp.Enabled = true;
+            this.scanApp.Interval = 1000;
+            this.scanApp.Tick += new System.EventHandler(this.scanApp_Tick);
+            // 
+            // bapp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 202);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "bapp";
+            this.Text = "Windowsx64";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer scanApp;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading;
+
+namespace bapp
+{
+    public partial class bapp : Form
+    {
+        public bapp()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, out uint ProcessId);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        string GetProcessNameOfActiveApp()
+        {
+            IntPtr hwnd = GetForegroundWindow();
+            uint pid;
+            GetWindowThreadProcessId(hwnd, out pid);
+            Process processActive = Process.GetProcessById((int)pid);
+            return processActive.ProcessName;
+        }
+
+        int GetProcessIdOfActiveApp()
+        {
+            IntPtr hwnd = GetForegroundWindow();
+            uint pid;
+            GetWindowThreadProcessId(hwnd, out pid);
+            Process processActive = Process.GetProcessById((int)pid);
+            return processActive.Id;
+        }
+
+        private void scanApp_Tick(object sender, EventArgs e)
+        {
+            string procName = GetProcessNameOfActiveApp();
+            if (procName == "chrome" || procName == "iexplore" || procName == "firefox" || procName == "opera")
+            {
+                Process proc = Process.GetProcessById(GetProcessIdOfActiveApp());
+                proc.Kill();
+            }
+        }
+    }
+}
